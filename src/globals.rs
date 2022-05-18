152c0887460409ccec601b5c5b0da067ca41fa54
fina <code@felinira.net>
2022-05-18 16:35:42 +0200

    Allow to specify relay url as environment variable

--- src/globals.rs
+++ src/globals.rs
@@ -4,11 +4,17 @@ use regex::Regex;
 use wormhole::transfer::AppVersion;
 use wormhole::{AppConfig, AppID};
 
-pub const WORMHOLE_RENDEZVOUS_RELAY: &str = "ws://relay.magic-wormhole.io:4000/v1";
+pub static WORMHOLE_RENDEZVOUS_RELAY: Lazy<String> = Lazy::new(|| {
+    if let Some(url) = option_env!("RELAY_URL") {
+        url.to_string()
+    } else {
+        "ws://relay.magic-wormhole.io:4000/v1".to_string()
+    }
+});
 pub const WORMHOLE_TRANSIT_RELAY: &str = "tcp://transit.magic-wormhole.io:4001";
 pub static WORMHOLE_APPCFG: Lazy<AppConfig<AppVersion>> = Lazy::new(|| AppConfig {
     id: AppID::new("lothar.com/wormhole/text-or-file-xfer"),
-    rendezvous_url: WORMHOLE_RENDEZVOUS_RELAY.into(),
+    rendezvous_url: (&*(WORMHOLE_RENDEZVOUS_RELAY)).into(),
     app_version: AppVersion {},
 });
 
